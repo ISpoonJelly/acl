d1031ec1775ee8ee7e34588d4bcd78754465abff
ISpoonJelly <tiitomoha@gmail.com>
2015-12-13 16:22:09 +0200

    -fixed an exception in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ActiveRecord::Base
   has_many :posts, foreign_key: 'id'
   has_many :comments, foreign_key: 'id'
   has_many :messages, foreign_key: 'id'
-  //
+  
   has_many :friendships, foreign_key: 'id'
   has_many :friends, :through => :friendships, foreign_key: 'id'
-  //
+  
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user, foreign_key: 'id'
   has_many :follow_posts, foreign_key: 'id'
@@ -23,7 +23,8 @@ class User < ActiveRecord::Base
 
   def that_born_on_is_not_in_the_future 
     self.errors.add :date_of_birth, 'is in the future' 
-    unless self.date_of_birth <= Date.today 
+      unless self.date_of_birth <= Date.today 
+    end
   end
 
 end
